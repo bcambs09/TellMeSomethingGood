32e35ad171a886ed97b336addbd01d6cc6cef3a9
Brendan Campbell <bdcam@umich.edu>
2017-06-04 22:28:24 -0500

    Change name to WhatsGoodToday

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>What's Good Today?</title>
+    </head>
+    <body>
+        <h1>What's Good Today?</h1>
+        <form action="postMessage.php" method="POST">
+            Message:<br>
+            <input type="text" name="message">
+            <br><br>
+            <input type="submit" value="Post Message">
+        </form>
+    </body>
+</html>
